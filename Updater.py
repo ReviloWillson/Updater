62f5f019a584a4e03c4ed1c895d8b022fa7421bd
ReviloWillson <79640254+ReviloWillson@users.noreply.github.com>
2021-12-31 16:00:39 +0000

    Program

--- Updater.py
+++ Updater.py
@@ -1,5 +1,8 @@
 #If you are using this on a Rasperry Pi OS based system, run the command sudo pip install popen. Install pip if required with command sudo install pip. 
 
+#Updater  Simple Linux-only update script
+#Copyright (C) 2021  Oliver Willson
+
 import os, sys
 
 USER = os.environ.get('SUDO_USER')
